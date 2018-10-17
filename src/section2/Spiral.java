0a308d6925706a77c637f3564bad3b8717ef6bf4
omarangulomcms <44244159+omarangulomcms@users.noreply.github.com>
2018-10-17 13:57:26 -0700

    Add files via upload

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,22 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Farengar=new Robot();
 		// 5. Set your robot's pen down 
-		
+		Farengar.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		Farengar.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 0; i < 75 ; i++) {
 		
 			// 7. Change the pen color to random
-		
+		Farengar.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			Farengar.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		Farengar.turn(360/7);
 			// 8. Set the pen width to i
-			
+		Farengar.setPenWidth(i);
+		}
 	}
 }
